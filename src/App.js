512ed743bcebf798807ea0cb328298c26ac28a4e
makingthingsforfriends <yuwei.s@hotmail.co.uk>
2022-03-16 11:12:52 +1300

    Extracted ToDoList to separate component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,11 @@
 import "./App.css";
+import ToDoList from "./components/ToDoList";
 
 function App() {
-  const toDos = [
-    { type: "task", title: "do homework", completed: false },
-    { type: "task", title: "make dinner", completed: false },
-    {
-      type: "list",
-      tasks: [
-        { type: "task", title: "chop potatoes", completed: false },
-        { type: "task", title: "wash tomatoes", completed: false },
-        { type: "task", title: "bake veggies", completed: false },
-        {
-          type: "list",
-          tasks: [
-            { type: "task", title: "30 min potatoes", completed: false },
-            { type: "task", title: "heat leftovers", completed: false },
-          ],
-        },
-      ],
-    },
-    { type: "task", title: "wash dishes", completed: false },
-  ];
-
-  function renderToDos(toDos) {
-    const toDoTree = toDos.map((task) => {
-      if (task.type === "task") {
-        return (
-          <div className="list-toDo">
-            <span className="title">{task.title}</span>
-          </div>
-        );
-      } else {
-        return <div className="list">{renderToDos(task.tasks)}</div>;
-      }
-    });
-    return <div className="list">{toDoTree}</div>;
-  }
-
   return (
     <div className="App">
       <h1>All my to-dos</h1>
-      {renderToDos(toDos)}
+      <ToDoList />
     </div>
   );
 }
---
+++ src/components/ToDoList.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function ToDoList() {
+  const toDos = [
+    { type: "task", title: "do homework", completed: false },
+    { type: "task", title: "make dinner", completed: false },
+    {
+      type: "list",
+      tasks: [
+        { type: "task", title: "chop potatoes", completed: false },
+        { type: "task", title: "wash tomatoes", completed: false },
+        { type: "task", title: "bake veggies", completed: false },
+        {
+          type: "list",
+          tasks: [
+            { type: "task", title: "30 min potatoes", completed: false },
+            { type: "task", title: "heat leftovers", completed: false },
+          ],
+        },
+      ],
+    },
+    { type: "task", title: "wash dishes", completed: false },
+  ];
+
+  function renderToDos(toDos) {
+    const toDoTree = toDos.map((task) => {
+      if (task.type === "task") {
+        return (
+          <div className="list-toDo">
+            <span className="title">{task.title}</span>
+          </div>
+        );
+      } else {
+        return <div className="list">{renderToDos(task.tasks)}</div>;
+      }
+    });
+    return <div className="list">{toDoTree}</div>;
+  }
+
+  return <div>{renderToDos(toDos)}</div>;
+}
